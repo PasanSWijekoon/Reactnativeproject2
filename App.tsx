45c5cbefa403a7552345ab281a94cdfecb7efcab
pasan <pasanwijekoon673@gmail.com>
2023-01-28 17:50:18 +0000

    first commit

---
+++ App.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import {Image, SafeAreaView, StyleSheet, Text, View} from 'react-native';
+
+function App() {
+  const ui = (
+    <SafeAreaView style={styles.main}>
+      <View style={styles.sub}>
+        <Text>My First Priject</Text>
+       
+      </View>
+    </SafeAreaView>
+  );
+  return ui;
+}
+
+export default App;
+
+const styles = StyleSheet.create({
+  main: {
+    flex: 1,
+    backgroundColor: 'red',
+  },
+  sub: {
+    flex: 1,
+    // backgroundColor: 'blue',
+  },
+});
